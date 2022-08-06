b5c88cc1e73ba018e329e42bc3064ccbc4125e2c
Jesper Friis <jesper.friis@sintef.no>
2022-08-06 18:32:41 +0200

    Updated demo6

--- examples/scripts/demo6-postgresql-1.py
+++ examples/scripts/demo6-postgresql-1.py
@@ -1,10 +1,26 @@
-"""Demonstrate use of a postgresql database
+"""Demonstrate use of a postgresql database.
+
+This script writes an instance to postgres.sintef.no.
+See https://web.yammer.com/main/threads/eyJfdHlwZSI6IlRocmVhZCIsImlkIjoiMTE4Njc5NjY0ODY2OTE4NCJ9
+
+Will only work for users that have been granted access to the database.
+
+Linux users must authorize themselves with kinit before running this script:
+
+    $ kinit jesperf@SINTEF.NO
+
 """
 from pathlib import Path
 
 import dlite
 
 
+# Database setup
+host = 'postgres.sintef.no'
+user = 'jesperf@sintef.no'  # Change this line to your username
+database = 'jesperf-bigmap'
+
+# Paths
 thisdir = Path(__file__).resolve().parent
 entitydir = thisdir.parent / 'entities'
 outputdir = thisdir / 'output'
@@ -22,8 +38,4 @@ data = dlite.Instance.from_location(
 )
 
 # Save to postgresql DB
-host = 'postgres.sintef.no'
-user = 'jesperf@sintef.no'
-database = 'jesperf-bigmap'
-
-data.save(f'postgresql://{host}?{user=};{database=};{password=}')
+data.save(f'postgresql://{host}?{user=};{database=}')
--- examples/scripts/demo6-postgresql-2.py
+++ examples/scripts/demo6-postgresql-2.py
@@ -1,8 +1,20 @@
 """Demonstrate use of a postgresql database
+
+This script reads an instance from postgres.sintef.no.
+
+See demo6-postgresql-1.py for connection details.
 """
+from pathlib import Path
+
 import dlite
 
 
+# Database setup
+host = 'postgres.sintef.no'
+user = 'jesperf@sintef.no'  # Change this line to your username
+database = 'jesperf-bigmap'
+
+# Paths
 thisdir = Path(__file__).resolve().parent
 entitydir = thisdir.parent / 'entities'
 outputdir = thisdir / 'output'
@@ -13,8 +25,4 @@ ontodir = thisdir.parent / 'ontologies'
 dlite.storage_path.append(f'{entitydir}/BatteryCycleData.json')
 
 # Load from postgresql db
-host = 'postgres.sintef.no'
-user = 'jesperf@sintef.no'
-database = 'jesperf-bigmap'
-
 dlite.Instance.from_url(f'postgresql://{host}?{user=};{database=}')
